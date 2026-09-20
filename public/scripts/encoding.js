1021ec11320aad7bbfa6ecd7c36df1cfd0a6c587
Zac Delventhal <contrib@zacis.me>
2026-09-20 15:31:56 -0500

    Refactor clocker byte format to use checkpoints
    This is a fairly sizable change to the way clocker strings record
    absolute time. Previously, groups contained an absolute created
    timestamp in minutes, and all clock ins were relative to that.
    
    Now groups have no created time (I didn't really care about it anyway),
    and clock ins are always saved as the difference between themselves and
    the next chronological timestamp (i.e. the preceding timestamp in terms
    of data order). Before the first clock in and any time a clock in is
    more than 2^24 seconds after the preceding one (~6 months), an absolute
    checkpoint timestamp is inserted.
    
    This prevents clock in byte size from growing as we get further away
    from when the group was first created. In practice, a clock in should
    stay at a fairly stable 6-7 bytes in size, growing larger as needed to
    handle extreme cases.
    
    A side benefit is it also means each byte string will be timestamped,
    which will help in prompting to the user when conflicts need to be
    resolved.

--- public/scripts/encoding.js
+++ public/scripts/encoding.js
@@ -1,140 +1,132 @@
-const FORMAT_VERSION = 0;
-const GROUP_TYPE = 0;
+const CURRENT_FORMAT_VERSION = 0;
+
+const CHECKPOINT_TYPE = 0;
 const TIME_TYPE = 1;
-const TIME_SUB_TYPE = 0;
+
+// Note that since groups cheat and use seven bits for label size,
+// they effectively have a data type of BOTH 2 and 3
+const GROUP_TYPE = 2;
+
+const MAX_CLOCK_IN_SIZE = 3;
+const MAX_SECOND_GAP = 2 ** (MAX_CLOCK_IN_SIZE * 8) - 1;
 
 /**
- * Encodes an array of groups and clock in/out times as a base64 clocker string.
+ * Encodes clock in/out times and their groups as a base64 clocker string.
  * 
  * Clocker strings are a custom binary format. The first byte is the version
- * number. That is followed by a list of interspersed group info and clock
- * in/out times. These are ordered from newest to oldest, with each group coming
- * just before the first time that references it. In this way, clocker strings
- * can be truncated at any point to fit size requirements (such as for a URL
- * query string) and the oldest data will be dropped without generating errors.
+ * number, followed by an absolute checkpoint timestamp from the time the string
+ * was encoded. After this initial metadata, there is an interspersed list of
+ * clock in/out times, group info, and additional checkpoints. These are ordered
+ * from newest to oldest, with each group coming just before the first time that
+ * references it, and checkpoints coming before times which are more than 2^24
+ * seconds apart. In this way, clocker strings can be truncated at any point to
+ * fit size requirements, such as for a URL query string. The oldest data can
+ * simply be dropped without generating errors.
  *
- * Group info byte format:
+ * Checkpoint timestamp byte format:
  *   - Header (1 byte):
- *     - Data Type (1 bit): Always zero
- *     - Label Size (7 bits): Byte size of string label (0-127)
- *   - UUID (16 bytes)
- *   - Creation Time (4 bytes): Two's complement signed unix epoch minutes
- *   - Color (3 bytes): RGB color
- *   - Label (0-127 bytes): A UTF-8 string label
+ *     - Data Type (2 bits): Always zero
+ *     - Timestamp size (2 bits): Byte size of timestamp (4-7)
+ *     - Timestamp sign (1 bit): Before (one) or after (zero) Unix epoch
+ *     - Unused (3 bits): Zeroed out, may be used for future metadata
+ *   - Timestamp (4-7 bytes): Seconds removed from Unix epoch
  *
  * Clock in/out time byte format:
  *   - Header (1 byte):
- *     - Data Type (1 bit): Always one
- *     - Data Sub-type (1 bit): Always zero
- *     - Group Index Size (2 bits): Byte size of group index (1-4)
- *     - Clock In Size (2 bits): Byte size of clock in timestamp (3-6)
- *     - Clock Out Size (2 bits) Byte size of clock out timestamp (1-4)
- *   - Group Index (1-4 bytes): A 1-based index matching a group above
- *   - Clock In (3-6 bytes): Number of seconds since group creation
- *   - Clock Out (1-4 bytes): Number of seconds since clock in
+ *     - Data Type (2 bits): Always one
+ *     - Group Index Size (2 bits): Byte size of group index (0-3)
+ *     - Clock In Size (2 bits): Byte size of clock in timestamp (0-3)
+ *     - Clock Out Size (2 bits) Byte size of clock out timestamp (2-5)
+ *   - Group Index (0-3 bytes): Order based index of linked group
+ *   - Clock In (0-3 bytes): Seconds before preceding clock in or checkpoint
+ *   - Clock Out (2-5 bytes): Seconds since this clock in
+ *
+ * Group info byte format:
+ *   - Header (1 byte):
+ *     - Data Type (1 bit): Always one (i.e. 2 or 3 when sized as two bits)
+ *     - Label Size (7 bits): Byte size of string label (0-127)
+ *   - UUID (16 bytes)
+ *   - Color (3 bytes): RGB color
+ *   - Label (0-127 bytes): A UTF-8 string label
  */
-export function encode(groups, times) {
-  const groupsById = Object.fromEntries(groups.map(grp => [grp.id, grp]));
-
+export function encode(timestamp, times, groups) {
   // Sort times from newest to oldest
-  const sortedTimes = times.toSorted((a, b) => {
-    const aTime = groupsById[a.group].created + a.in;
-    const bTime = groupsById[b.group].created + b.in;
-    return bTime - aTime;
-  });
+  const sortedTimes = times.toSorted((a, b) => b.in - a.in);
 
-  // Sort groups from oldest associated time to newest so we can pop groups off
+  // Sort groups from newest associated time to oldest
   const sortedGroups = groups.toSorted((a, b) => {
     const aIndex = sortedTimes.findIndex(time => time.group === a.id);
     const bIndex = sortedTimes.findIndex(time => time.group === b.id);
-    return bIndex - aIndex;
+    return aIndex - bIndex;
   });
-
   const groupIndexesById = Object.fromEntries(sortedGroups.map((grp, i) => [grp.id, i]));
 
-  const data = [];
+  const data = [
+    new Uint8Array([CURRENT_FORMAT_VERSION]),
+    encodeCheckpoint(timestamp),
+  ];
 
-  for (const time of sortedTimes) {
-    if (time.group === sortedGroups.at(-1).id) {
-      data.push({
-        type: GROUP_TYPE,
-        data: sortedGroups.pop()
-      });
-    }
-    data.push({
-      type: TIME_TYPE,
-      data: time,
-    });
+  let nextTimestamp = timestamp;
+  let groupIndex = 0;
+
+  // If first clock in time is from after our encode timestamp,
+  // we need a future checkpoint to match
+  if (sortedTimes[0] && sortedTimes[0].in > nextTimestamp) {
+    data.push(encodeCheckpoint(sortedTimes[0].in));
+    nextTimestamp = sortedTimes[0].in;
   }
 
-  const headerBytes = new Uint8Array([FORMAT_VERSION]);
-  const dataBytes = data.map(({ type, data }) => {
-    switch (type) {
-    case GROUP_TYPE:
-      return encodeGroup(data);
-    case TIME_TYPE:
-      return encodeTime(groupIndexesById[data.group], groupsById[data.group], data);
+  for (const time of sortedTimes) {
+    if (nextTimestamp - time.in > MAX_SECOND_GAP) {
+      data.push(encodeCheckpoint(time.in));
+      nextTimestamp = time.in;
     }
-  });
 
-  return bytesToBase64(concatBytes([headerBytes, ...dataBytes]));
+    if (groupIndex < sortedGroups.length && time.group === sortedGroups[groupIndex].id) {
+      data.push(encodeGroup(sortedGroups[groupIndex]));
+      groupIndex += 1;
+    }
+
+    data.push(encodeTime(nextTimestamp, groupIndexesById[time.group], time));
+    nextTimestamp = time.in;
+  }
+
+  return bytesToBase64(concatBytes(data));
 }
 
-/**
- * Converts an encoded clocker string into an array of group info objects and
- * an array of clock in/out time objects. The two arrays are returned in
- * a tuple with group info first.
- *
- * Group Info object properties:
- *   - id: UUID string formatted with the lowercase dashed convention
- *   - color: RGB color hex string
- *   - label: String label
- *
- * Clock in/out time object properties:
- *   - group: UUID string of group time is under
- *   - in: Clock in time as unix epoch milliseconds (rounded to nearest second)
- *   - out: Undefined if time has not been clocked out, otherwise clock out time
- *     as unix epoch milliseconds (rounded to nearest second)
- */
-export function decode(encodedString) {
+function encodeCheckpoint(timestamp) {
+  const deltaSeconds = Math.abs(msToSeconds(timestamp));
+  const tsBytes = uintToDynamicBytes(deltaSeconds, 7, 4);
 
-}
-
-function encodeGroup({ id, created, color, label }) {
-  const idBytes = hexToFixedBytes(id, 16);
-  const createdBytes = intToFixedBytes(msToMinutes(created), 4);
-  const colorBytes = hexToFixedBytes(color, 3);
-  const labelBytes = textToDynamicBytes(label, 127);
-
-  const typeBits = GROUP_TYPE << 7;
-  const headerBytes = new Uint8Array([typeBits + labelBytes.length]);
+  const headerBytes = concatBits([
+    CHECKPOINT_TYPE << 6,
+    tsBytes.length - 4 << 4,
+    // Should maybe save timestamp as signed integer, but dynamically truncating
+    // two's complement numbers is a pain so I'll just use this bit as a sign
+    timestamp < 0 ? 1 : 0 << 3
+  ]);
 
   return concatBytes([
     headerBytes,
-    idBytes,
-    createdBytes,
-    colorBytes,
-    labelBytes
+    tsBytes
   ]);
 }
 
-function encodeTime(groupIndex, group, time) {
-  const indexBytes = uintToDynamicBytes(groupIndex, 4, 1);
+function encodeTime(nextTimestamp, groupIndex, time) {
+  const indexBytes = uintToDynamicBytes(groupIndex, 3);
 
-  const inSeconds = msToSeconds(time.in - group.created);
-  const inBytes = uintToDynamicBytes(inSeconds, 6, 3);
+  const nextTsSeconds = msToSeconds(nextTimestamp);
+  const inSeconds = msToSeconds(time.in);
+  const outSeconds = msToSeconds(time.out ?? time.in);
 
-  const outSeconds = msToSeconds((time.out ?? time.in) - time.in);
-  const outBytes = uintToDynamicBytes(outSeconds, 4, 1);
+  const inBytes = uintToDynamicBytes(nextTsSeconds - inSeconds, MAX_CLOCK_IN_SIZE);
+  const outBytes = uintToDynamicBytes(outSeconds - inSeconds, 5, 2);
 
-  const typeBits = TIME_TYPE << 7;
-  const subTypeBits = TIME_SUB_TYPE << 6;
-  const indexSizeBits = indexBytes.length - 1 << 4;
-  const inSizeBits = inBytes.length - 3 << 2;
-  const outSizeBits = outBytes.length - 1;
-
-  const headerBytes = new Uint8Array([
-    typeBits + subTypeBits + indexSizeBits + inSizeBits + outSizeBits
+  const headerBytes = concatBits([
+    TIME_TYPE << 6,
+    indexBytes.length << 4,
+    inBytes.length << 2,
+    outBytes.length - 2
   ]);
 
   return concatBytes([
@@ -145,49 +137,51 @@ function encodeTime(groupIndex, group, time) {
   ]);
 }
 
-function msToSeconds(ms) {
-  return Math.floor(ms / 1000);
+function encodeGroup({ id, color, label }) {
+  const idBytes = hexToFixedBytes(id, 16);
+  const colorBytes = hexToFixedBytes(color, 3);
+  const labelBytes = textToDynamicBytes(label, 127);
+
+  // Note that the 7-bit label size will overlap with the data type by one bit.
+  // This only works because GROUP_TYPE is a number with a zero in the last bit.
+  const headerBytes = concatBits([
+    GROUP_TYPE << 6,
+    labelBytes.length
+  ]);
+
+  return concatBytes([
+    headerBytes,
+    idBytes,
+    colorBytes,
+    labelBytes
+  ]);
 }
 
-function msToMinutes(ms) {
-  return Math.floor(ms / 60000);
+function msToSeconds(ms) {
+  return Math.floor(ms / 1000);
 }
 
 function clamp(value, max, min = 0) {
   return Math.max(Math.min(value, max), min);
 }
 
-function clampBits(value, bitSize) {
-  const amplitude = 2 ** (bitSize - 1);
-  return clamp(value, amplitude - 1, -amplitude);
-}
+function uintToDynamicBytes(positiveInteger, maxByteSize, minByteSize = 0) {
+  // There is no point in using more than seven bytes with JS numbers.
+  // They lose precision before you get to the eighth byte. If I need
+  // larger integers, I will need to add support for BigInt arguments.
+  const clampedMaxSize = clamp(maxByteSize, 7);
+  const clampedMinSize = clamp(minByteSize, 7);
 
-function clampUnsignedBits(value, bitSize) {
-  return clamp(value, 2 ** bitSize - 1);
-}
-
-function intToFixedBytes(integer, byteSize) {
-  const clampedSize = clamp(byteSize, 8);
-  const sanitized = clampBits(Math.trunc(integer), clampedSize * 8);
-
-  const dataView = new DataView(new ArrayBuffer(8));
-  dataView.setBigInt64(0, BigInt(sanitized));
-
-  return new Uint8Array(dataView.buffer.slice(-clampedSize));
-}
-
-function uintToDynamicBytes(unsignedInteger, maxByteSize, minByteSize = 0) {
-  const clampedMaxSize = clamp(maxByteSize, 8);
-  const clampedMinSize = clamp(minByteSize, 8);
-  const sanitized = clampUnsignedBits(Math.trunc(unsignedInteger), clampedMaxSize * 8);
+  const maxValue = Math.min(2 ** (clampedMaxSize * 8) - 1, Number.MAX_SAFE_INTEGER);
+  const sanitized = clamp(Math.round(positiveInteger), maxValue);
 
   const dataView = new DataView(new ArrayBuffer(8));
   dataView.setBigUint64(0, BigInt(sanitized));
   const bytes = new Uint8Array(dataView.buffer);
 
-  const start = bytes.findIndex(byte => byte > 0);
   const minFrom = bytes.length - clamp(maxByteSize, bytes.length);
   const maxFrom = bytes.length - clamp(minByteSize, bytes.length);
+  const start = sanitized === 0 ? maxFrom : bytes.findIndex(byte => byte > 0);
   const from = clamp(start > 0 ? start : maxFrom, maxFrom, minFrom);
 
   return bytes.slice(from);
@@ -205,6 +199,11 @@ function hexToFixedBytes(hexString, byteSize) {
 function textToDynamicBytes(utf8String, maxByteSize) {
   const encoder = new TextEncoder();
   return encoder.encode(utf8String).slice(0, maxByteSize);
+}
+
+function concatBits(arrayOfOffsetBits) {
+  const byte = arrayOfOffsetBits.reduce((sum, bits) => sum + bits, 0);
+  return new Uint8Array([byte]);
 }
 
 function concatBytes(arrayOfBytes) {
